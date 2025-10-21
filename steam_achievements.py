28b025d72767e8b7ffbd950240f72a64a06fc816
Ismoh <12631485+Ismoh@users.noreply.github.com>
2025-10-21 18:38:31 +0200

    feat: add steam achievements script

---
+++ steam_achievements.py
@@ -0,0 +1,17 @@
+# Are you a fan of Steam achievements?
+# No problem.
+# So am I!
+# Just start this script once and you're done!
+
+
+def hello_world():
+    # Ensure you pay attention to the Getting Started screen to access the First Program page.
+    harvest()
+
+
+def main():
+    hello_world()
+
+
+if __name__ == "__main__":
+    clear()
